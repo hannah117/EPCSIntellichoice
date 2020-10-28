ab52dd5bb57882c4b5f1da6f74281ba6888fd803
Hannah <hannahismail711@gmail.com>
2020-10-28 15:22:14 -0500

    Start of tutor dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import React, { Component } from 'react';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import QuestionList from './components/QuestionList';
 
 class App extends Component{
   render(){
     return(
       <div className="App">
-        <h1>Hello</h1>
+      <QuestionList/>
     </div>
     );
   }
